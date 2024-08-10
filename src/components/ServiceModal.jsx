c8c8498e3bc7ab868cf5ccd9e4b72e111a5eb084
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-08-10 16:44:17 +0800

    Style: modle sytle and background blur

--- src/components/ServiceModal.jsx
+++ src/components/ServiceModal.jsx
@@ -1,11 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const ServiceModal = ({ isOpen, onClose, title, info, imgURL, duration }) => {
   if (!isOpen) return null;
 
   return (
-    <div>
-      <div className="relative bg-white w-[50rem] opacity-80 p-7 rounded-[30px]">
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center backdrop-blur-md z-40">
+      <div className="relative bg-white w-[50rem] opacity-80 p-7 rounded-[30px] z-50">
         <h2 className="text-[3rem] text-tertiary">{title}</h2>
         <p className="text-secondary text-[1.1rem]">{info}</p>
         <div className="flex items-center">
@@ -22,7 +22,10 @@ const ServiceModal = ({ isOpen, onClose, title, info, imgURL, duration }) => {
                 </li>
               ))}
           </ul>
-          <button className="p-5 bg-primary text-highLight rounded-full m-2 " > Booking Now !</button>
+          <button className="p-5 bg-primary text-highLight rounded-full m-2 ">
+            {" "}
+            Booking Now !
+          </button>
         </div>
         <button
           onClick={onClose}
