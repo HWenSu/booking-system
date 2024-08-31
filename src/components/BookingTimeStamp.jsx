6bf4eceeb6f52f6f43481585fc80e74cfcb1ce80
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-08-31 13:32:33 +0800

    Change: BookingTimeStamp style

--- src/components/BookingTimeStamp.jsx
+++ src/components/BookingTimeStamp.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
-import { setHours, setMinutes } from "date-fns";
 
 const BookingTimeStamp = () => {
-  const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(), 30), 17)
-  );
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <DatePicker
       selected={startDate}
       onChange={(date) => setStartDate(date)}
       showTimeSelect
-      minTime={setHours(setMinutes(new Date(), 0), 17)}
-      maxTime={setHours(setMinutes(new Date(), 30), 20)}
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      timeCaption="time"
       dateFormat="MMMM d, yyyy h:mm aa"
     />
   );
