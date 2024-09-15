6241a56ce4ff2714158606d504496db75c15683e
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-09-15 23:15:32 +0800

    Feat: Staff page API

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -1,42 +1,48 @@
 import useAPIService from '../components/hooks/useAPIService';
 
 const Staff = () => {
-  const {data} = useAPIService('/modals/staffData.json')
+  const { data } = useAPIService("http://localhost:5000/staff");
 
   return (
     <div>
       <ul className='flex flex-col w-[80vw]  mx-auto my-[8vh]'>
         { data && 
-        data.staff.map( (s) => {
-          return(
-          <li key={s.id} className='flex justify-around m-[2vh] p-4 bg-white/50 rounded-lg'>
-            <div
-            style={{ backgroundImage:`url(${s.url})` }}
-            className={`w-[10rem] h-[10rem] rounded-full bg-[length:12rem] bg-no-repeat bg-left`}
+        data.map( (s) => {
+          return (
+            <li
+              key={s.id}
+              className="flex justify-around m-[2vh] p-4 bg-white/50 rounded-lg"
             >
-            </div>
-              <div className='w-[40rem] text-tertiary'>
-                <div className='flex items-center'>
-                  <h2 className='font-semibold text-[2rem] text-highLight mr-7'>{s.name.toUpperCase()}</h2>
-                  <p>{s.gender.toUpperCase()}</p>
+              <div
+                style={{ backgroundImage: `url(${s.img})` }}
+                className={`w-[10rem] h-[10rem] rounded-full bg-[length:12rem] bg-no-repeat bg-left`}
+              ></div>
+              <div className="w-[40rem] text-tertiary">
+                <div className="flex items-center">
+                  <h2 className="font-semibold text-[2rem] text-highLight mr-7">
+                    {s.name}
+                  </h2>
+                  <p>{s.gender}</p>
                 </div>
-                
-                <div className='flex my-4'>
-                  {s.expertise.map((e, index)=> {
-                    return <h3 key={index} className='mr-3 p-2 rounded-full bg-primary' >{e.toUpperCase()}</h3>
+
+                <div className="flex my-4">
+                  <h3 className="mr-3 p-2 rounded-full bg-primary">
+                    {s.expertise}
+                  </h3>
+                </div>
+                <div className="flex bg-white rounded-full opacity-50 p-5">
+                  {s.certificates.map((certificate, index) => {
+                    return (
+                      <h3 key={index} className="mr-5">
+                        {" "}
+                        {certificate}
+                      </h3>
+                    );
                   })}
                 </div>
-                <div className='flex bg-white rounded-full opacity-50 p-5'>
-                {Object.keys(s.certificates).map((key, index) => {
-                  return(
-                    <h3 key={index} className='mr-5'> {key.toUpperCase() + ": " + s.certificates[key]}</h3>
-                  ) 
-                })}
-                </div>
               </div>
-             
             </li>
-            )
+          );
           })
         }
 
