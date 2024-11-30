c960c1c17dab11d70dc135c1f24771312e803d6c
Elva Su <112391401+HWenSu@users.noreply.github.com>
2024-11-30 16:18:52 +0800

    Featch: create POST API when booking form submitted

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,26 +1,66 @@
 import { useState } from 'react'
 import SubmittedModal from './SubmittedModal';
+import axios from "axios"
+
+//Post API URL
+const postURL = 'http://localhost:5000/miumiu-spa/order'
 
 const SubmitButton = ({formData, trigger}) => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  //設定為已提交
+  const submitData = {
+    serviceId: formData.Service_id,
+    start: formData.startTime,
+    end: formData.endTime,
+    staff: formData.Staff_id,
+    name: formData.Name,
+    gender: formData.Gender_id,
+    phone: formData.Phone,
+    email: formData.Email,
+    remark: formData.Remark,
+  };
+
+  //串接 post API 將預約資料傳回後端
+  const postBookingData = async (data) => {
+    try {
+      const response = await fetch(postURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log("Booking Success", result);
+      return result;
+    } catch (error) {}
+    console.log("Error submitting booking", error);
+    throw error;
+  };
+
+  //按下提交表單時
   const handleSubmit = async () => {
     //驗證表單是否正確填入
     const valid = await trigger();
-    if (!valid) {
-    console.log("Validation Errors:", errors)
-    return; // 阻止送出
-    } else {
-      setIsSubmitted(true)
-    }}
+
+    const result = await postBookingData(submitData);
+    console.log("API Response", result)
     
-  console.log(formData)
+
+    if (!valid) {
+      console.log("Validation Errors:", errors);
+      return; // 阻止送出
+    } else {
+      setIsSubmitted(true);
+    }
+  };
+
+  console.log(formData);
   console.log(isSubmitted);
 
   return (
     <div>
-      <button type='button' onClick={handleSubmit} className="submitBtn">
+      <button type="button" onClick={handleSubmit} className="submitBtn">
         Submit
       </button>
       {/* 點擊後彈出提視窗 */}
