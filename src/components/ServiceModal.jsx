4c8bef0aa67d87159c80f0f7fecc88267cfcf3e5
Elva Su <112391401+HWenSu@users.noreply.github.com>
2025-01-12 22:45:13 +0800

    Style: service modal style update

--- src/components/ServiceModal.jsx
+++ src/components/ServiceModal.jsx
@@ -14,43 +14,44 @@ const ServiceModal = ({ isOpen, onClose, title, description, imgURL, selectedMas
   
   return (
     <div className="modalBg">
-      <div className="modalPanel m-10 relative">
-        <h2 className="text-[10vw] md:text-[3rem]">{title}</h2>
-        <p className="p-2">{description}</p>
-        <div className="flex flex-col  justify-around md:flex-row items-center">
-          <img
-            src={imgURL}
-            alt={title}
-            className="w-[9rem] rounded-full m-3 md:w-[12rem]"
-          />
-          <ul className="grid text-primary md:grid-cols-2 gap-2">
-            {data &&
-              data.map((item) => {
-                if (selectedMassage_id === item.massage_id) {
-                  return (
-                    <li
-                      key={item.duration}
-                      className="flex p-3 bg-tertiary rounded-full m-2 justify-between items-center"
-                    >
-                      <div className="flex items-center m-1">
-                        <p className="text-[7vw] m-1 md:text-[3rem]">{item.duration}</p>
-                        <p>mins</p>
-                      </div>
-                      <button className="text-white bg-highlight rounded-full p-3 hover:bounce2">
-                        <Link to="/miumiu-spa/orders">$ {item.price}</Link>
-                      </button>
-                    </li>
-                  );
-                }
-              })}
-          </ul>
-        </div>
-        <button
-          onClick={onClose}
-          className="absolute bg-tertiary right-0 top-0  w-[3rem] h-[3rem] text-white rounded-full text-[1.5rem] text-center"
-        >
-          x
+      <div className="modalPanel">
+        <button onClick={onClose} className="modalBtn">
+          X
         </button>
+        <div className="modalBtnContainer">
+          <h2 className="text-[10vw] md:text-[3rem]">{title}</h2>
+          <p className="p-2">{description}</p>
+          <div className="flex flex-col  justify-around md:flex-row items-center">
+            <img
+              src={imgURL}
+              alt={title}
+              className="w-[9rem] rounded-full m-3 md:w-[12rem]"
+            />
+            <ul className="grid text-primary md:grid-cols-2 gap-2">
+              {data &&
+                data.map((item) => {
+                  if (selectedMassage_id === item.massage_id) {
+                    return (
+                      <li
+                        key={item.duration}
+                        className="flex p-3 bg-tertiary rounded-full m-2 justify-between items-center"
+                      >
+                        <div className="flex items-center m-1">
+                          <p className="text-[7vw] m-1 md:text-[3rem]">
+                            {item.duration}
+                          </p>
+                          <p>mins</p>
+                        </div>
+                        <button className="text-white bg-highlight rounded-full p-3 hover:bounce2">
+                          <Link to="/miumiu-spa/orders">$ {item.price}</Link>
+                        </button>
+                      </li>
+                    );
+                  }
+                })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
